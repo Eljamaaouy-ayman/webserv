7abe60b097fb7360d7ceca03757ee17a42446e52
Ayman El Jamaaouy <ael-jama@c4r4p10.1337.ma>
2026-06-15 23:58:18 +0100

    request class added

--- srcs/includes/request.hpp
+++ srcs/includes/request.hpp
@@ -4,6 +4,31 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include "server.hpp"
+#include <algorithm>
+#include <arpa/inet.h>
+#include <bits/stdc++.h>
+#include <climits>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <dirent.h>
+#include <fcntl.h>
+#include <fstream>
+#include <iostream>
+#include <netinet/in.h>
+#include <sstream>
+#include <sys/epoll.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <wait.h>
+#include <vector>
+#include <map>
+#define PORT 8080
+#define IP INADDR_ANY
+#define IPv4 AF_INET
+#define MAX_BUFFER_SIZE 4096
+#define MAX_EVENTS 1024
 
 typedef struct location {
     std::string path;
@@ -29,6 +54,19 @@ class ConfigFile {
 
         void parse_config_file(char *av);
 };
+class Request : public Webserv {
+
+  private:
+    std::map<std::string, std::string> request;
+    std::map<std::string, std::string> session;
+	std::string	_cgiResponse;
+
+  public:
+    METHOD method;
+    std::string path;
+    std::string httpV;
+    bool isCGI;
+    ConfigFile config;
 
     // * CGI information
     struct CgiInfo {
@@ -45,6 +83,29 @@ class ConfigFile {
       size_t contentLength;
       std::string contentType;
     };
+
+    // * obj of cgi struct
+    CgiInfo cgi;
+
+
+    // * Default Contructor
+    Request();
+
+    void setRequest(const std::string &req);
+    const std::map<std::string, std::string> &getRequest() const;
+
+    void setSession(const std::string session_id, const std::string value);
+    const std::map<std::string, std::string> &getSession() const;
+    
+    void checkCGI(std::string path);
+    const bool &getIsCGI() const;
+
+    bool pathGCIisFile(std::string path);
+	void	setCgiResponse(const std::string& cgiResponse);
+	const std::string& getCgiResponse() const;
+	std::string			getMethodByName(int enumFlag);
+};
+
 
 class Request{
     public:
